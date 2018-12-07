c648c181ac06dd67abdbe36ecf3df361b73fffa3
BA Aliou <alioubafr@gmail.com>
2018-12-07 12:22:46 +0100

    Update install_openshift_centos.sh

--- openshift/install_openshift_centos.sh
+++ openshift/install_openshift_centos.sh
@@ -10,5 +10,8 @@ export PATH=$PATH:/usr/local/bin/
 rm -rf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit*
 git clone https://github.com/gshipley/installcentos
 cd installcentos
+export DOMAIN=cluster1.dakar.io
+export USERNAME=cluster1
+export PASSWORD=sonateltraining
 ./install-openshift.sh
 oc login -u root -p password https://console.cluster1.dakar.io:8443/
